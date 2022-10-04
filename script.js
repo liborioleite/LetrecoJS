36264a823aeae722ce0b5992313ba761c72ac290
Antonio <tonny858895>
2022-10-04 13:46:59 -0300

    alterações muito importantes

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let palavraDoDia = "carro";
 
-
 function letreco() {
    
    let valor1 = document.getElementById("valorA").value;
@@ -9,65 +8,6 @@ function letreco() {
    let valor4 = document.getElementById("valorD").value;
    let valor5 = document.getElementById("valorE").value;
    
-   if (valor1 === palavraDoDia[0]) {
-      document.getElementById("valorA").style.backgroundColor = "#008000"; // inputs na cor verde 
-   }
-   if (valor2 === palavraDoDia[1]) {
-      document.getElementById("valorB").style.backgroundColor = "#008000"; // inputs na cor verde
-   }
-   if (valor3 === palavraDoDia[2]) {
-      document.getElementById("valorC").style.backgroundColor = "#008000"; // inputs na cor verde
-   }
-   if (valor4 === palavraDoDia[3]) {
-      document.getElementById("valorD").style.backgroundColor = "#008000"; // inputs na cor verde
-   }
-   if (valor5 === palavraDoDia[4]) {
-      document.getElementById("valorE").style.backgroundColor = "#008000"; // inputs na cor verde
-   }
-
-   for (let i; i < palavraDoDia.length; i++) {
-      if (valor1 === palavraDoDia[i]) {
-         if (i !== 0) {
-            document.getElementById("valorA").style.backgroundColor = "#FFFF00" // inputs na cor amarela
-         }
-      }
-   }
-
-   for (let i; i < palavraDoDia.length; i++) {
-      if (valor2 === palavraDoDia[i]) {
-         if (i !== 1) {
-            document.getElementById("valorB").style.backgroundColor = "#FFFF00" // inputs na cor amarela
-         }
-      }
-   }
-
-   for (let i; i < palavraDoDia.length; i++) {
-      if (valor3 === palavraDoDia[i]) {
-         if (i !== 2) {
-            document.getElementById("valorC").style.backgroundColor = "#FFFF00" // inputs na cor amarela
-         }
-      }
-   }
-
-   for (let i; i < palavraDoDia.length; i++) {
-      if (valor4 === palavraDoDia[i]) {
-         if (i !== 3) {
-            document.getElementById("valorD").style.backgroundColor = "#FFFF00" // inputs na cor amarela
-         }
-      }
-   }
-
-   for (let i; i < palavraDoDia.length; i++) {
-      if (valor5 === palavraDoDia[i]) {
-         if (i !== 4) {
-            document.getElementById("valorE").style.backgroundColor = "#FFFF00" // inputs na cor amarela
-         }
-      }
-   }
-
-   /* OS 5 IFS ABAIXO SERVEM PRA COMPARAR OS INPUTS, E CONCLUIR QUE SE ELES NÃO FOREM NEM VERDES, NEM AMARELOS, 
-   ENTÃO DEVEM SER PINTADOS DE VERMELHO. */
-
    if (document.getElementById("valorA").style.backgroundColor !== "#FFFF00" && document.getElementById("valorA").style.backgroundColor !== "008000") {
       document.getElementById("valorA").style.backgroundColor = "#FF0000";
    }
@@ -82,6 +22,62 @@ function letreco() {
    }
    if (document.getElementById("valorE").style.backgroundColor !== "#FFFF00" && document.getElementById("valorE").style.backgroundColor !== "008000") {
       document.getElementById("valorE").style.backgroundColor = "#FF0000";
+   }
+   
+   for (let i = 0; i < palavraDoDia.length; i++) {
+      if (valor1 === palavraDoDia[i]) {
+         if (i !== 0) {
+            document.getElementById("valorA").style.backgroundColor = "#FFFF00" // inputs na cor amarela
+         }
+      }
+   }
+
+   for (let i = 0; i < palavraDoDia.length; i++) {
+      if (valor2 === palavraDoDia[i]) {
+         if (i !== 1) {
+            document.getElementById("valorB").style.backgroundColor = "#FFFF00" // inputs na cor amarela
+         }
+      }
+   }
+
+   for (let i = 0; i < palavraDoDia.length; i++) {
+      if (valor3 === palavraDoDia[i]) {
+         if (i !== 2) {
+            document.getElementById("valorC").style.backgroundColor = "#FFFF00" // inputs na cor amarela
+         }
+      }
+   }
+
+   for (let i = 0; i < palavraDoDia.length; i++) {
+      if (valor4 === palavraDoDia[i]) {
+         if (i !== 3) {
+            document.getElementById("valorD").style.backgroundColor = "#FFFF00" // inputs na cor amarela
+         }
+      }
+   }
+
+   for (let i = 0; i < palavraDoDia.length; i++) {
+      if (valor5 === palavraDoDia[i]) {
+         if (i !== 4) {
+            document.getElementById("valorE").style.backgroundColor = "#FFFF00" // inputs na cor amarela
+         }
+      }
+   }
+
+   if (valor1 === palavraDoDia[0]) {
+      document.getElementById("valorA").style.backgroundColor = "#008000"; // inputs na cor verde 
+   }
+   if (valor2 === palavraDoDia[1]) {
+      document.getElementById("valorB").style.backgroundColor = "#008000"; // inputs na cor verde
+   }
+   if (valor3 === palavraDoDia[2]) {
+      document.getElementById("valorC").style.backgroundColor = "#008000"; // inputs na cor verde
+   }
+   if (valor4 === palavraDoDia[3]) {
+      document.getElementById("valorD").style.backgroundColor = "#008000"; // inputs na cor verde
+   }
+   if (valor5 === palavraDoDia[4]) {
+      document.getElementById("valorE").style.backgroundColor = "#008000"; // inputs na cor verde
    }
 
 }
